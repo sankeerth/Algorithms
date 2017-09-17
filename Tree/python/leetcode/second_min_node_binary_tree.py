7770199eb6d80445aada28617f50b9914628a05b
Sankeerth <sankeerth456@gmail.com>
2017-09-16 22:38:41 -0400

    Added simple leetcode solution to '671. Second Minimum Node In a Binary Tree'

--- Tree/python/leetcode/second_min_node_binary_tree.py
+++ Tree/python/leetcode/second_min_node_binary_tree.py
@@ -58,3 +58,22 @@ print(sol.findSecondMinimumValue(deserialize('[2,2,4,2,2,null,null,2,2,2,2,2,2,2
 print(sol.findSecondMinimumValue(deserialize('[2,2,5,null,null,5,7]')))
 print(sol.findSecondMinimumValue(deserialize('[2,2,2]')))
 print(sol.findSecondMinimumValue(deserialize('[2]')))
+
+"""
+Leetcode solution:
+
+def findSecondMinimumValue(self, root):
+    self.ans = float('inf')
+    min1 = root.val
+
+    def dfs(node):
+        if node:
+            if min1 < node.val < self.ans:
+                self.ans = node.val
+            elif node.val == min1:
+                dfs(node.left)
+                dfs(node.right)
+
+    dfs(root)
+    return self.ans if self.ans < float('inf') else -1
+"""

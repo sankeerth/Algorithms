fdac047a5fa957a7295f14953d58f59272f5db0b
Sankeerth <sankeerth456@gmail.com>
2017-10-23 00:09:56 -0400

    Updated Remove Nth node from end of Linked list to use helper functions from common folder

--- LinkedList/python/leetcode/remove_nth_node_from_end_of_list.py
+++ LinkedList/python/leetcode/remove_nth_node_from_end_of_list.py
@@ -11,36 +11,7 @@ Note:
 Given n will always be valid.
 Try to do this in one pass.
 """
-
-
-class ListNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-
-def build_linked_list(array):
-    if not array:
-        return None
-
-    head = ListNode(array[0])
-    temp = head
-    for val in array[1:]:
-        temp.next = ListNode(val)
-        temp = temp.next
-
-    return head
-
-
-def print_linked_list(head):
-    if not head:
-        return None
-    array = list()
-    while head:
-        array.append(head.val)
-        head = head.next
-
-    print(array)
+from LinkedList.python.common.linked_list_operations import ListNode, build_linked_list, print_linked_list
 
 
 class Solution(object):

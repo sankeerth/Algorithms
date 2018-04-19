f6c5f553802f75eff326a928fb29dfe46f081a4d
Sankeerth Sreeramachandra <ssreeramachand@egencia.com>
2018-04-19 17:01:42 -0500

    Added problem: Unique Binary Search Trees

---
+++ Tree/python/leetcode/unique_binary_search_trees.py
@@ -0,0 +1,46 @@
+"""
+96. Unique Binary Search Trees
+
+Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
+
+Example:
+
+Input: 3
+Output: 5
+Explanation:
+Given n = 3, there are a total of 5 unique BST's:
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+"""
+
+import math
+
+
+class Solution(object):
+    # Catalan Number  (2n)!/((n+1)!*n!)
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return math.factorial(2 * n) / (math.factorial(n) * math.factorial(n + 1))
+
+    # DP
+    def numTrees1(self, n):
+        res = [0] * (n + 1)
+        res[0] = 1
+        for i in range(1, n + 1):
+            for j in range(i):
+                res[i] += res[j] * res[i - 1 - j]
+        return res[n]
+
+
+sol = Solution()
+print(sol.numTrees1(2))
+print(sol.numTrees1(3))
+print(sol.numTrees1(4))
+print(sol.numTrees1(5))

5b86ceb7ae07ef4b08ea1238f50c6884bd45d489
Sankeerth Sreeramachandra <ssreeramachand@egencia.com>
2018-05-12 12:17:37 -0500

    Added problem: Distinct Subsequences

---
+++ String/python/leetcode/distinct_subsequences.py
@@ -0,0 +1,84 @@
+"""
+115. Distinct Subsequences
+
+Given a string S and a string T, count the number of distinct subsequences of S which equals T.
+
+A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
+
+Example 1:
+
+Input: S = "rabbbit", T = "rabbit"
+Output: 3
+Explanation:
+
+As shown below, there are 3 ways you can generate "rabbit" from S.
+(The caret symbol ^ means the chosen letters)
+
+rabbbit
+^^^^ ^^
+rabbbit
+^^ ^^^^
+rabbbit
+^^^ ^^^
+Example 2:
+
+Input: S = "babgbag", T = "bag"
+Output: 5
+Explanation:
+
+As shown below, there are 5 ways you can generate "bag" from S.
+(The caret symbol ^ means the chosen letters)
+
+babgbag
+^^ ^
+babgbag
+^^    ^
+babgbag
+^    ^^
+babgbag
+  ^  ^^
+babgbag
+    ^^^
+"""
+
+
+class Solution:
+    def numDistinct(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: int
+        """
+        if not t:
+            return 1
+        elif not s:
+            return 0
+
+        dp = [[0] * len(s) for _ in range(len(t))]
+
+        dp[0][0] = 1 if s[0] == t[0] else 0
+        for i in range(1, len(s)):
+            if s[i] == t[0]:
+                dp[0][i] = 1 + dp[0][i-1]
+            else:
+                dp[0][i] = dp[0][i-1]
+
+        for i in range(1, len(t)):
+            dp[i][0] = 0
+
+        for i in range(1, len(t)):
+            for j in range(1, len(s)):
+                if t[i] == s[j]:
+                    dp[i][j] = dp[i][j-1] + dp[i-1][j-1]
+                else:
+                    dp[i][j] = dp[i][j-1]
+
+        return dp[len(t)-1][len(s)-1]
+
+
+sol = Solution()
+print(sol.numDistinct("rabbbit", "rabbit"))
+print(sol.numDistinct("babgbag", "bag"))
+print(sol.numDistinct("", "a"))
+print(sol.numDistinct("a", "r"))
+print(sol.numDistinct("a", ""))

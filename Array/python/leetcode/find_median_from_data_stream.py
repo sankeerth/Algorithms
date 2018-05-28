313a67ff9f0daa8b966f2e2b1f58a5b0cc274ba1
Sankeerth Sreeramachandra <ssreeramachand@egencia.com>
2018-05-28 16:29:30 -0500

    Added problem: Find Median from Data Stream

---
+++ Array/python/leetcode/find_median_from_data_stream.py
@@ -0,0 +1,139 @@
+"""
+295. Find Median from Data Stream
+
+Median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value. So the median is the mean of the two middle value.
+
+For example,
+[2,3,4] , the median is 3
+
+[2,3], the median is (2 + 3) / 2 = 2.5
+
+Design a data structure that supports the following two operations:
+
+void addNum(int num) - Add a integer number from the data stream to the data structure.
+double findMedian() - Return the median of all elements so far.
+Example:
+
+addNum(1)
+addNum(2)
+findMedian() -> 1.5
+addNum(3)
+findMedian() -> 2
+"""
+import heapq
+
+
+class MedianFinder(object):
+    """
+    Another really good solution with BST approach, although addNum and findNum both have O(log(N)) runtime.
+    https://leetcode.com/problems/find-median-from-data-stream/discuss/74166/Solution-using-Binary-Search-Tree
+    """
+    """
+    The idea is to use two heaps (one max heap, one mn heap) to save the input data. firstQ is a max_heap to save the
+    first half of the data with smaller values, and secQ is a min_heap to save the second half of the data with bigger
+    values. Everytime when inserting a new value, we first compare if it is smaller than the top of firstQ (the largest
+    value of the first half), if so, insert into firstQ. Otherwise, it belongs to the second half. After inserting,
+    we have to balance the first half and the second half to make sure either they have the same length or the length
+    difference is only 1. The median will be the mean of two top elements (when they have the same length) or the
+    top element of the queue with a larger length.
+    """
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.lower_half = list()  # max heap
+        self.upper_half = list()  # min heap
+
+    def addNum(self, num):
+        """
+        :type num: int
+        :rtype: void
+        """
+        if len(self.lower_half) == 0 or num < -self.lower_half[0]:
+            heapq.heappush(self.lower_half, -num)
+        else:
+            heapq.heappush(self.upper_half, num)
+
+        if len(self.lower_half) > len(self.upper_half) + 1:
+            top = heapq.heappop(self.lower_half)
+            heapq.heappush(self.upper_half, -top)
+        elif len(self.upper_half) > len(self.lower_half) + 1:
+            top = heapq.heappop(self.upper_half)
+            heapq.heappush(self.lower_half, -top)
+
+    def findMedian(self):
+        """
+        :rtype: float
+        """
+        if len(self.lower_half) == len(self.upper_half):
+            if len(self.lower_half) == 0:
+                return 0
+            else:
+                return (self.upper_half[0] - self.lower_half[0])/2
+        else:
+            return (-self.lower_half[0]) if len(self.lower_half) > len(self.upper_half) else self.upper_half[0]
+
+
+med = MedianFinder()
+med.addNum(6)
+print(med.findMedian())
+med.addNum(10)
+print(med.findMedian())
+med.addNum(2)
+print(med.findMedian())
+med.addNum(6)
+print(med.findMedian())
+med.addNum(5)
+print(med.findMedian())
+med.addNum(0)
+print(med.findMedian())
+med.addNum(6)
+print(med.findMedian())
+med.addNum(3)
+print(med.findMedian())
+med.addNum(1)
+print(med.findMedian())
+med.addNum(0)
+print(med.findMedian())
+med.addNum(0)
+print(med.findMedian())
+
+# print(med.findMedian())
+# med.addNum(2)
+# print(med.findMedian())
+# med.addNum(5)
+# print(med.findMedian())
+# med.addNum(4)
+# print(med.findMedian())
+# med.addNum(1)
+# print(med.findMedian())
+# med.addNum(7)
+# print(med.findMedian())
+
+
+"""
+leetcode discuss solution:
+Supporting both min- and max-heap is more or less cumbersome, depending on the language, so I simply negate the numbers 
+in the heap in which I want the reverse of the default order. To prevent this from causing a bug with -231 
+(which negated is itself, when using 32-bit ints), I use integer types larger than 32 bits.
+
+from heapq import *
+
+class MedianFinder:
+
+    def __init__(self):
+        self.heaps = [], []
+
+    def addNum(self, num):
+        small, large = self.heaps
+        heappush(small, -heappushpop(large, num))
+        if len(large) < len(small):
+            heappush(large, -heappop(small))
+
+    def findMedian(self):
+        small, large = self.heaps
+        if len(large) > len(small):
+            return float(large[0])
+        return (large[0] - small[0]) / 2.0
+"""

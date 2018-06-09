ba08e3c96e132e68c6334334ae1ee7c832e7a8e1
Sankeerth Sreeramachandra <ssreeramachand@egencia.com>
2018-06-09 17:34:43 -0500

    Added dp recursive solution for 'Interleaving String'

--- String/python/leetcode/interleaving_string.py
+++ String/python/leetcode/interleaving_string.py
@@ -59,3 +59,48 @@ sol = Solution()
 print(sol.isInterleave("ab", "cbd", "acbbd"))
 print(sol.isInterleave("aabcc", "dbbca", "aadbbcbcac"))
 print(sol.isInterleave("aabcc", "dbbca", "aadbbbaccc"))
+
+
+'''
+My dp recursive (top-down) solution:
+
+class Solution:
+    def isInterleave(self, s1, s2, s3):
+        """
+        :type s1: str
+        :type s2: str
+        :type s3: str
+        :rtype: bool
+        """
+        if not s1:
+            return s2 == s3
+        elif not s2:
+            return s1 == s3
+
+        if len(s1) + len(s2) != len(s3):
+            return False
+
+        dp = [[float('inf')] * (len(s2)+1) for _ in range(len(s1)+1)]
+
+        def interleave_recr(i, j):
+            if i == len(s1) and j == len(s2):
+                return True
+            elif dp[i][j] != float('inf'):
+                return dp[i][j]
+            elif i == len(s1):
+                dp[i][j] = s2[j] == s3[i+j] and interleave_recr(i, j+1)
+            elif j == len(s2):
+                dp[i][j] = s1[i] and s3[i+j] and interleave_recr(i+1, j)
+            elif s1[i] == s2[j] == s3[i+j]:
+                dp[i][j] = interleave_recr(i+1, j) or interleave_recr(i, j+1)
+            elif s1[i] == s3[i+j]:
+                dp[i][j] = interleave_recr(i+1, j)
+            elif s2[j] == s3[i+j]:
+                dp[i][j] = interleave_recr(i, j+1)
+            else:
+                return False
+
+            return dp[i][j]
+
+        return interleave_recr(0, 0)
+'''

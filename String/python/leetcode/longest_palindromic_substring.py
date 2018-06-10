4550f79d92635a8fb9416f3101ec7206c24f52ce
Sankeerth Sreeramachandra <ssreeramachand@egencia.com>
2018-06-09 23:52:37 -0500

    Added dp recursive solution for 'Longest Palindromic Substring'

--- String/python/leetcode/longest_palindromic_substring.py
+++ String/python/leetcode/longest_palindromic_substring.py
@@ -33,7 +33,7 @@ class Solution(object):
 
             return j + 1, k - 1
 
-        for i in range(1, l):
+        for i in range(l):
             j, k = len_of_palindrome(i - 1, i + 1, l)
             if (k - j + 1) > max_len:
                 max_len = k - j + 1
@@ -84,5 +84,49 @@ class Solution(object):
                 else:
                     dp[i][j] = False
 
+        return result
+'''
+
+'''
+My dp recursive (top-down) solution:
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if not s:
+            return ""
+
+        result = s[0]
+        max_len = 1
+
+        dp = [[float('inf')] * len(s) for _ in range(len(s))]
+
+        def longest_palidrome_recr(i, j):
+            nonlocal result
+            nonlocal max_len
+
+            if dp[i][j] != float('inf'):
+                return dp[i][j]
+            elif i == j:
+                dp[i][j] = True
+            elif i > j:
+                dp[i][j] = True
+            elif s[i] == s[j]:
+                longest_palidrome_recr(i + 1, j - 1) or longest_palidrome_recr(i, j - 1) or longest_palidrome_recr(i + 1, j)
+                dp[i][j] = dp[i+1][j-1]
+            else:
+                longest_palidrome_recr(i, j-1) or longest_palidrome_recr(i+1, j)
+                dp[i][j] = False
+
+            if dp[i][j] is True and j-i+1 > max_len:
+                max_len = j-i+1
+                result = s[i:j+1]
+
+            return dp[i][j]
+
+        longest_palidrome_recr(0, len(s)-1)
         return result
 '''

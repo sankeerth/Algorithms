0af243482a96107aa0745751b0e7b99090df4f43
Sankeerth V S <sankeerth456@gmail.com>
2021-04-24 17:01:04 -0700

    Update remove_nth_node_from_end_of_list.py

--- LinkedList/python/leetcode/remove_nth_node_from_end_of_list.py
+++ LinkedList/python/leetcode/remove_nth_node_from_end_of_list.py
@@ -10,20 +10,18 @@ For example,
 Note:
 Given n will always be valid.
 Try to do this in one pass.
+
+Constraints:
+The number of nodes in the list is sz.
+1 <= sz <= 30
+0 <= Node.val <= 100
+1 <= n <= sz
 """
 from LinkedList.python.common.linked_list_operations import ListNode, build_linked_list, print_linked_list
 
 
 class Solution(object):
     def removeNthFromEnd(self, head, n):
-        """
-        :type head: ListNode
-        :type n: int
-        :rtype: ListNode
-        """
-        if not head:
-            return head
-
         fast = head
         slow = head
         prev_slow = None
@@ -50,3 +48,31 @@ sol = Solution()
 print_linked_list(sol.removeNthFromEnd(build_linked_list([1,2,3,4,5]), 3))
 print_linked_list(sol.removeNthFromEnd(build_linked_list([1,2,3,4,5]), 1))
 print_linked_list(sol.removeNthFromEnd(build_linked_list([1,2,3,4,5]), 5))
+
+
+"""
+Leetcode solution: A little simpler solution using Dummy node as the first node.
+
+The first pointer advances the list by n+1n+1 steps from the beginning, while the second pointer starts 
+from the beginning of the list. Now, both pointers are exactly separated by nn nodes apart. 
+We maintain this constant gap by advancing both pointers together until the first pointer arrives past the last node. 
+The second pointer will be pointing at the nnth node counting from the last. 
+We relink the next pointer of the node referenced by the second pointer to point to the node's next next node.
+
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        dummy = ListNode(0)
+        dummy.next = head
+        first, second = dummy, dummy
+
+        for _ in range(n+1):
+            first = first.next
+
+        while first:
+            first = first.next
+            second = second.next
+        
+        second.next = second.next.next
+        return dummy.next
+"""

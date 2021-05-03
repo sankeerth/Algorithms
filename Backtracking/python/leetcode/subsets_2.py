4cd1d706494eda1b35d591a001244602f1ee5a84
Sankeerth V S <sankeerth456@gmail.com>
2021-05-02 21:20:45 -0700

    Update subsets_2.py

--- Backtracking/python/leetcode/subsets_2.py
+++ Backtracking/python/leetcode/subsets_2.py
@@ -20,27 +20,66 @@ If nums = [1,2,2], a solution is:
 
 class Solution(object):
     def subsetsWithDup(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-
-        result = list()
-        current = list()
+        result, current = [], []
         nums.sort()
 
-        def subsets_recr(i, l):
+        def subsets_recr(i):
             result.append(list(current))
-            for j in range(i, l):
+            for j in range(i, len(nums)):
                 if i == j or nums[j] != nums[j-1]:
                     current.append(nums[j])
-                    subsets_recr(j+1, l)
+                    subsets_recr(j+1)
                     current.pop()
 
-        subsets_recr(0, len(nums))
-
+        subsets_recr(0)
         return result
+
 
 sol = Solution()
 print(sol.subsetsWithDup([1, 2, 2]))
 print(sol.subsetsWithDup([1, 1, 2, 2]))
+
+
+"""
+Modification of 'Either include the digit or not'.
+
+class Solution(object):
+    def subsets(self, nums):
+        res = []
+        nums.sort()
+
+        def subsetsRecursive(i, sub):
+            if i == len(nums):
+                res.append(sub)
+                return
+
+            subsetsRecursive(i+1, sub)
+            # ignore individual repeated elements, for ex: [2] from [1, 2, 2, 2]
+            if not sub and i > 0 and nums[i] == nums[i-1]:
+                return
+            # stop repeating subsets and taking it further, for ex: [1, 2] from [1, 2, 2, 2]
+            if sub and nums[i] != sub[-1] and nums[i] == nums[i-1]:
+                return
+            subsetsRecursive(i+1, sub + [nums[i]])
+
+        subsetsRecursive(0, [])
+        return res
+"""
+
+"""
+Iterative solution:
+
+class Solution(object):
+    def subsets(self, nums):
+        res, s = [[]], set()
+
+        for num in nums:
+            for i in range(len(res)):
+                sub = list(res[i])
+                sub.append(num)
+                if set(sub) not in s:
+                    res.append(sub)
+                    s.union(set(sub))
+
+        return res
+"""

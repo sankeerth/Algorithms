7ca5b4ab41a7bd4e12d12553b7f55e3ec293761f
Sankeerth V S <sankeerth456@gmail.com>
2021-05-14 16:45:46 -0700

    Update distinct_subsequences.py
    115. Distinct Subsequences

--- String/python/leetcode/distinct_subsequences.py
+++ String/python/leetcode/distinct_subsequences.py
@@ -2,33 +2,29 @@
 115. Distinct Subsequences
 
 Given a string S and a string T, count the number of distinct subsequences of S which equals T.
-
-A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
+A subsequence of a string is a new string which is formed from the original string by deleting 
+some (can be none) of the characters without disturbing the relative positions of the remaining 
+characters. (ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
 
 Example 1:
-
 Input: S = "rabbbit", T = "rabbit"
 Output: 3
 Explanation:
-
 As shown below, there are 3 ways you can generate "rabbit" from S.
 (The caret symbol ^ means the chosen letters)
-
 rabbbit
 ^^^^ ^^
 rabbbit
 ^^ ^^^^
 rabbbit
 ^^^ ^^^
-Example 2:
 
+Example 2:
 Input: S = "babgbag", T = "bag"
 Output: 5
 Explanation:
-
 As shown below, there are 5 ways you can generate "bag" from S.
 (The caret symbol ^ means the chosen letters)
-
 babgbag
 ^^ ^
 babgbag
@@ -39,16 +35,15 @@ babgbag
   ^  ^^
 babgbag
     ^^^
+
+Constraints:
+1 <= s.length, t.length <= 1000
+s and t consist of English letters.
 """
 
 
 class Solution:
-    def numDistinct(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: int
-        """
+    def numDistinct(self, s: str, t: str) -> int:
         if not t:
             return 1
         elif not s:
@@ -58,13 +53,7 @@ class Solution:
 
         dp[0][0] = 1 if s[0] == t[0] else 0
         for i in range(1, len(s)):
-            if s[i] == t[0]:
-                dp[0][i] = 1 + dp[0][i-1]
-            else:
-                dp[0][i] = dp[0][i-1]
-
-        for i in range(1, len(t)):
-            dp[i][0] = 0
+            dp[0][i] = dp[0][i-1] + int(s[i] == t[0])
 
         for i in range(1, len(t)):
             for j in range(1, len(s)):
@@ -84,36 +73,28 @@ print(sol.numDistinct("a", "r"))
 print(sol.numDistinct("a", ""))
 
 
-'''
+"""
 My dp recursive (top-down) solution:
 
 class Solution:
-    def numDistinct(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: int
-        """
-        if not t:
-            return 1
-        elif not s:
-            return 0
-
-        dp = [[float('inf')] * (len(t)+1) for _ in range(len(s)+1)]
-
-        def distinct_recr(i, j):
-            if dp[i][j] != float('inf'):
-                return dp[i][j]
-            if len(s) - i < len(t) - j:
-                dp[i][j] = 0
-            elif j == len(t):
-                dp[i][j] = 1
-            elif s[i] == t[j]:
-                dp[i][j] = distinct_recr(i+1, j+1) + distinct_recr(i+1, j)
+    def numDistinct(self, s: str, t: str) -> int:
+        memo = {}
+        def numDistinctRecursive(i, j):
+            if j == len(t):
+                return 1
+            elif len(s) - i < len(t) - j:
+                return 0
+            elif (i, j) in memo:
+                return memo[(i, j)]
+            
+            res = 0
+            if s[i] == t[j]:
+                res = numDistinctRecursive(i+1, j+1) + numDistinctRecursive(i+1, j)
             else:
-                dp[i][j] = distinct_recr(i+1, j)
+                res = numDistinctRecursive(i+1, j)
+            
+            memo[(i, j)] = res
+            return res
 
-            return dp[i][j]
-
-        return distinct_recr(0, 0)
-'''
+        return numDistinctRecursive(0, 0)
+"""

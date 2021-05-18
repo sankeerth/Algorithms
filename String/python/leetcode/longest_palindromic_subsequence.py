4bf0ccabe76890d7e7b70bc85f7ba467237f4113
Sankeerth V S <sankeerth456@gmail.com>
2021-05-17 23:31:53 -0700

    Update longest_palindromic_subsequence.py

--- String/python/leetcode/longest_palindromic_subsequence.py
+++ String/python/leetcode/longest_palindromic_subsequence.py
@@ -1,48 +1,48 @@
 """
 516. Longest Palindromic Subsequence
 
-Given a string s, find the longest palindromic subsequence's length in s. You may assume that the maximum length of s is 1000.
+Given a string s, find the longest palindromic subsequence's length in s.
+
+A subsequence is a sequence that can be derived from another sequence by deleting 
+some or no elements without changing the order of the remaining elements.
 
 Example 1:
 Input:
-
 "bbbab"
 Output:
 4
 One possible longest palindromic subsequence is "bbbb".
+
 Example 2:
 Input:
-
 "cbbd"
 Output:
 2
 One possible longest palindromic subsequence is "bb".
+
+Constraints:
+    1 <= s.length <= 1000
+    s consists only of lowercase English letters.
 """
 
 
-class Solution(object):
-    def longestPalindromeSubseq(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
         if not s:
             return 0
 
-        length = len(s)
-        dp = [([1] * length) for _ in range(length)]
+        dp = [([0] * len(s)) for _ in range(len(s))]
+        for i in range(len(s)):
+            dp[i][i] = 1
 
-        for i in range(length-2, -1, -1):
-            for j in range(i+1, length):
+        for i in range(len(s)-2, -1, -1):
+            for j in range(i+1, len(s)):
                 if s[i] == s[j]:
-                    if j == i+1:
-                        dp[i][j] = 2
-                    else:
-                        dp[i][j] = dp[i+1][j-1] + 2
+                    dp[i][j] = dp[i+1][j-1] + 2
                 else:
                     dp[i][j] = max(dp[i][j-1], dp[i+1][j])
 
-        return dp[0][length-1]
+        return dp[0][len(s)-1]
 
 
 sol = Solution()

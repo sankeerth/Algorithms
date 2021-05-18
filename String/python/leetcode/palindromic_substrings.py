83c52587f8f209505fda35df0dcb68e6019ccc85
Sankeerth V S <sankeerth456@gmail.com>
2021-05-17 22:39:21 -0700

    Update palindromic_substrings.py

--- String/python/leetcode/palindromic_substrings.py
+++ String/python/leetcode/palindromic_substrings.py
@@ -8,39 +8,33 @@ Example 1:
 Input: "abc"
 Output: 3
 Explanation: Three palindromic strings: "a", "b", "c".
+
 Example 2:
 Input: "aaa"
 Output: 6
 Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+
+Constraints:
+1 <= s.length <= 1000
+s consists of lowercase English letters.
 """
 
-class Solution(object):
-    def countSubstrings(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        if not s:
-            return 0
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        res = len(s)
+        dp = [[False] * len(s) for _ in range(len(s))]
 
-        length = len(s)
-        dp = [([False] * length) for _ in range(length)]
-
-        for i in range(length):
+        for i in range(len(s)):
             dp[i][i] = True
 
-        result = length
-
-        for i in range(length-2, -1, -1):
-            for j in range(i+1, length):
+        for i in range(len(s)-2, -1, -1):
+            for j in range(i+1, len(s)):
                 if s[i] == s[j]:
                     if j == i+1 or dp[i+1][j-1]:
                         dp[i][j] = True
-                        result += 1
-                else:
-                    dp[i][j] = False
-
-        return result
+                        res += 1
+        
+        return res
 
 
 sol = Solution()
@@ -54,11 +48,7 @@ print(sol.countSubstrings("abdadba"))
 Another dp solution:
 
 class Solution(object):
-    def countSubstrings(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+    def countSubstrings(self, s: str) -> int:
         if not s:
             return 0
 

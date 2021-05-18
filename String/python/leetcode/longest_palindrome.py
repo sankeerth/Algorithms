dde0e9ba9ed36c1a4d9a387cd5bf6340fb7def73
Sankeerth V S <sankeerth456@gmail.com>
2021-05-17 22:45:59 -0700

    Update longest_palindrome.py

--- String/python/leetcode/longest_palindrome.py
+++ String/python/leetcode/longest_palindrome.py
@@ -1,33 +1,39 @@
 """
 409. Longest Palindrome
 
-Given a string which consists of lowercase or uppercase letters, find the length of the longest palindromes that can be built with those letters.
+Given a string which consists of lowercase or uppercase letters, 
+find the length of the longest palindromes that can be built with those letters.
 This is case sensitive, for example "Aa" is not considered a palindrome here.
 
 Note:
 Assume the length of given string will not exceed 1,010.
 
-Example:
-
+Example 1:
 Input:
 "abccccdd"
-
 Output:
 7
-
 Explanation:
 One longest palindrome that can be built is "dccaccd", whose length is 7.
+
+Example 2:
+Input: s = "a"
+Output: 1
+
+Example 3:
+Input: s = "bb"
+Output: 2
+
+Constraints:
+    1 <= s.length <= 2000
+    s consists of lowercase and/or uppercase English letters only.
 """
 
 from collections import Counter
 
 
-class Solution(object):
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
         if not s:
             return 0
 

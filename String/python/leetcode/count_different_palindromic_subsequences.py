c6d591f01121262cf4e164819d89a7ec1e925910
Sankeerth V S <sankeerth456@gmail.com>
2021-05-18 19:44:56 -0700

    Update count_different_palindromic_subsequences.py

--- String/python/leetcode/count_different_palindromic_subsequences.py
+++ String/python/leetcode/count_different_palindromic_subsequences.py
@@ -2,11 +2,8 @@
 730. Count Different Palindromic Subsequences
 
 Given a string S, find the number of different non-empty palindromic subsequences in S, and return that number modulo 10^9 + 7.
-
 A subsequence of a string S is obtained by deleting 0 or more characters from S.
-
 A sequence is palindromic if it is equal to the sequence reversed.
-
 Two sequences A_1, A_2, ... and B_1, B_2, ... are different if there is some i for which A_i != B_i.
 
 Example 1:
@@ -16,27 +13,24 @@ Output: 6
 Explanation:
 The 6 different non-empty palindromic subsequences are 'b', 'c', 'bb', 'cc', 'bcb', 'bccb'.
 Note that 'bcb' is counted only once, even though it occurs twice.
+
 Example 2:
 Input:
 S = 'abcdabcdabcdabcdabcdabcdabcdabcddcbadcbadcbadcbadcbadcbadcbadcba'
 Output: 104860361
 Explanation:
 There are 3104860382 different non-empty palindromic subsequences, which is 104860361 modulo 10^9 + 7.
-Note:
 
+Note:
 The length of S will be in the range [1, 1000].
 Each character S[i] will be in the set {'a', 'b', 'c', 'd'}.
 """
 
 class Solution:
-    def countPalindromicSubsequences(self, S):
-        """
-        :type S: str
-        :rtype: int
-        """
-        if not S:
+    def countPalindromicSubsequences(self, s: str) -> int:
+        if not s:
             return 0
-        length = len(S)
+        length = len(s)
         mod = 1000000007
         dp = [[0] * length for _ in range(length)]
 
@@ -45,15 +39,15 @@ class Solution:
 
         for i in range(length-2, -1, -1):
             for j in range(i+1, length):
-                if S[i] != S[j]:
+                if s[i] != s[j]:
                     dp[i][j] = (dp[i][j-1] + dp[i+1][j] - dp[i+1][j-1]) % mod
                 else:
                     dp[i][j] = (2 * dp[i+1][j-1]) % mod
                     l, r = i + 1, j-1
 
-                    while l <= r and S[i] != S[l]:
+                    while l <= r and s[i] != s[l]:
                         l += 1
-                    while l <= r and S[i] != S[r]:
+                    while l <= r and s[i] != s[r]:
                         r -= 1
 
                     if l > r:
@@ -69,7 +63,10 @@ class Solution:
 
 
 sol = Solution()
-print(sol.countPalindromicSubsequences("abdadba"))
+print(sol.countPalindromicSubsequences("bccb"))
+print(sol.countPalindromicSubsequences("babab"))
+print(sol.countPalindromicSubsequences("babac"))
 print(sol.countPalindromicSubsequences("aaaa"))
 print(sol.countPalindromicSubsequences("abdad"))
 print(sol.countPalindromicSubsequences("bdadb"))
+print(sol.countPalindromicSubsequences("abdadba"))

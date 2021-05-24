33b08be2c69d2949692129d23e3f84b7516c94b4
Sankeerth V S <sankeerth456@gmail.com>
2021-05-23 17:31:59 -0700

    Update search_in_rotated_sorted_array_2.py
    81. Search in Rotated Sorted Array II

--- Array/python/leetcode/search_in_rotated_sorted_array_2.py
+++ Array/python/leetcode/search_in_rotated_sorted_array_2.py
@@ -6,24 +6,18 @@ Suppose an array sorted in ascending order is rotated at some pivot unknown to y
 You are given a target value to search. If found in the array return true, otherwise return false.
 
 Example 1:
-
 Input: nums = [2,5,6,0,0,1,2], target = 0
 Output: true
 
 Example 2:
-
 Input: nums = [2,5,6,0,0,1,2], target = 3
 Output: false
 """
+from typing import List
 
 
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: bool
-        """
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
         lo, hi = 0, len(nums)-1
         while lo <= hi:
             mid = (lo + hi) // 2

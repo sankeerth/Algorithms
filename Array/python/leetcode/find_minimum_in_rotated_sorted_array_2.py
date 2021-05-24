2392ca284f0b9e6615da01ed47c8ba81ab7c12e9
Sankeerth V S <sankeerth456@gmail.com>
2021-05-23 22:49:54 -0700

    Update find_minimum_in_rotated_sorted_array_2.py
    154. Find Minimum in Rotated Sorted Array II

--- Array/python/leetcode/find_minimum_in_rotated_sorted_array_2.py
+++ Array/python/leetcode/find_minimum_in_rotated_sorted_array_2.py
@@ -14,15 +14,18 @@ Output: 1
 Example 2:
 Input: [2,2,2,0,1]
 Output: 0
+
+Constraints:
+n == nums.length
+1 <= n <= 5000
+-5000 <= nums[i] <= 5000
+nums is sorted and rotated between 1 and n times.
 """
+from typing import List
 
 
 class Solution(object):
-    def findMin(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+    def findMin(self, nums: List[int]) -> int:
         lo, hi = 0, len(nums)-1
         while lo < hi:
             if nums[lo] < nums[hi]:
@@ -39,7 +42,38 @@ class Solution(object):
 
 
 sol = Solution()
-print(sol.findMin([2, 2, 2, 0, 1]))
-print(sol.findMin([2, 2, 2, 0, 2]))
-print(sol.findMin([2, 0, 2, 2, 2]))
+print(sol.findMin([1,3,5]))
+print(sol.findMin([2,2,2,0,1]))
+print(sol.findMin([2,2,2,0,2]))
+print(sol.findMin([2,0,2,2,2]))
+print(sol.findMin([2,2,2,2,2,2,2,0,1,1,2,2]))
+print(sol.findMin([2,2,0,1,1,2,2,2,2,2,2,2]))
 
+
+"""
+My solution:
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        def binarySearch(lo, hi):
+            while lo < hi:
+                mid = (lo + hi) // 2
+                while lo < mid and nums[lo] == nums[mid]:
+                    lo += 1
+                while hi > mid and nums[hi] == nums[mid]:
+                    hi -= 1
+
+                mid = (lo + hi) // 2
+                if nums[mid] > nums[mid+1]:
+                    return mid+1
+
+                if nums[mid] <= nums[hi]:
+                    hi = mid
+                else:
+                    lo = mid + 1
+            return lo
+
+
+        index = binarySearch(0, len(nums)-1)
+        return nums[index]
+"""

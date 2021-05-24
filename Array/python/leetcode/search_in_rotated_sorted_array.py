3087ae0c76e2e3187a749c40f14cb87322f70010
Sankeerth V S <sankeerth456@gmail.com>
2021-05-23 17:13:09 -0700

    Update search_in_rotated_sorted_array.py
    33. Search in Rotated Sorted Array

--- Array/python/leetcode/search_in_rotated_sorted_array.py
+++ Array/python/leetcode/search_in_rotated_sorted_array.py
@@ -9,23 +9,18 @@ You may assume no duplicate exists in the array.
 Your algorithm's runtime complexity must be in the order of O(log n).
 
 Example 1:
-
 Input: nums = [4,5,6,7,0,1,2], target = 0
 Output: 4
-Example 2:
 
+Example 2:
 Input: nums = [4,5,6,7,0,1,2], target = 3
 Output: -1
 """
+from typing import List
 
 
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
         lo, hi = 0, len(nums)-1
         while lo <= hi:
             mid = (lo + hi) // 2
@@ -54,3 +49,56 @@ print(sol.search([4], 3))
 print(sol.search([3], 3))
 print(sol.search([3, 1], 1))
 print(sol.search([3, 1], 3))
+
+
+"""
+Leetcode solution by finding the index of minimum element first:
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        def find_rotate_index(left: int, right: int):
+            if nums[left] < nums[right]:
+                return 0
+
+            while left <= right:
+                pivot = (left + right) // 2
+                if nums[pivot] > nums[pivot + 1]:
+                    return pivot + 1
+                else:
+                    if nums[pivot] < nums[left]:
+                        right = pivot - 1
+                    else:
+                        left = pivot + 1
+
+        def search(left: int, right: int):
+            while left <= right:
+                pivot = (left + right) // 2
+                if nums[pivot] == target:
+                    return pivot
+                else:
+                    if target < nums[pivot]:
+                        right = pivot - 1
+                    else:
+                        left = pivot + 1
+            return -1
+
+        n = len(nums)
+
+        if n == 1:
+            return 0 if nums[0] == target else -1
+
+        rotate_index = find_rotate_index(0, n - 1)
+
+        # if target is the smallest element
+        if nums[rotate_index] == target:
+            return rotate_index
+        # if array is not rotated, search in the entire array
+        if rotate_index == 0:
+            return search(0, n - 1)
+        if target < nums[0]:
+            # search on the right side
+            return search(rotate_index, n - 1)
+        # search on the left side
+        return search(0, rotate_index)
+
+"""

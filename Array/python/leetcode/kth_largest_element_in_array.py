2be16a0f26bfa059046e82d4e4b355e5ab017353
Sankeerth V S <sankeerth456@gmail.com>
2021-05-24 14:13:53 -0700

    Update kth_largest_element_in_array.py

--- Array/python/leetcode/kth_largest_element_in_array.py
+++ Array/python/leetcode/kth_largest_element_in_array.py
@@ -4,25 +4,19 @@
 Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
 
 Example 1:
-
 Input: [3,2,1,5,6,4] and k = 2
 Output: 5
-Example 2:
 
+Example 2:
 Input: [3,2,3,1,2,4,5,5,6] and k = 4
 Output: 4
 """
 import random
+from heapq import heappush, heappop
 
 
 class Solution:
     def findKthLargest(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-
         def swap(i, j):
             temp = nums[i]
             nums[i] = nums[j]
@@ -49,7 +43,7 @@ class Solution:
 
         lo, hi = 0, len(nums)-1
         k = len(nums) - k
-        random.shuffle(nums)
+        #random.shuffle(nums)
 
         while lo < hi:
             index = partition(lo, hi)
@@ -65,6 +59,62 @@ class Solution:
 
 sol = Solution()
 print(sol.findKthLargest([3, 2, 1, 4, 6, 5], 2))
+print(sol.findKthLargest([3, 1, 2, 4, 5], 3))
+print(sol.findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4))
+print(sol.findKthLargest([3, 2, 1, 5, 6, 4], 2))
 print(sol.findKthLargest([1, 1, 1, 1, 2], 2))
 print(sol.findKthLargest([1, 1, 1, 1, 2], 1))
 print(sol.findKthLargest([1, 0, 0, 1, 0, 1, 0], 4))
+print(sol.findKthLargest([1, 3], 1))
+print(sol.findKthLargest([5, 3], 1))
+print(sol.findKthLargest([5, 3], 2))
+
+
+"""
+Just another solution of quick select:
+
+class Solution:
+    def findKthLargest(self, nums, k):
+        def swap(i, j):
+            nums[i], nums[j] = nums[j], nums[i]
+
+        def partition(lo, hi):
+            pivot = lo
+            while True:
+                while lo <= hi and nums[lo] <= nums[pivot]:
+                    lo += 1
+                while hi >= lo and nums[hi] >= nums[pivot]:
+                    hi -= 1
+
+                if lo > hi:
+                    break
+
+                swap(lo, hi)
+
+            swap(pivot, hi)
+            return hi
+
+        lo, hi, pivot = 0, len(nums)-1, -1
+        while pivot != len(nums)-k:
+            pivot = partition(lo, hi)
+            if pivot < len(nums)-k:
+                lo = pivot + 1
+            else:
+                hi = pivot - 1
+
+        return nums[pivot]
+"""
+
+"""
+O(n logk) solution using heap
+
+class Solution:
+    def findKthLargest(self, nums, k):
+        heap = []
+        for num in nums:
+            heappush(heap, num)
+            if len(heap) > k:
+                heappop(heap)
+
+        return heap[0]
+"""

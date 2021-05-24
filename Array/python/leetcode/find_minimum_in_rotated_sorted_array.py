d89c9defdd071bc473940bc9cdca5474dd81de8f
Sankeerth V S <sankeerth456@gmail.com>
2021-05-23 21:53:02 -0700

    Update find_minimum_in_rotated_sorted_array.py
    153. Find Minimum in Rotated Sorted Array

--- Array/python/leetcode/find_minimum_in_rotated_sorted_array.py
+++ Array/python/leetcode/find_minimum_in_rotated_sorted_array.py
@@ -4,26 +4,38 @@
 Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
 (i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
 
-Find the minimum element.
-You may assume no duplicate exists in the array.
+Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 
+1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+Given the sorted rotated array nums of unique elements, return the minimum element of this array.
 
 Example 1:
-
 Input: [3,4,5,1,2]
 Output: 1
-Example 2:
+Explanation: The original array was [1,2,3,4,5] rotated 3 times.
 
+Example 2:
 Input: [4,5,6,7,0,1,2]
 Output: 0
+Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+
+Example 3:
+Input: nums = [11,13,15,17]
+Output: 11
+Explanation: The original array was [11,13,15,17] and it was rotated 4 times. 
+
+Constraints:
+    n == nums.length
+    1 <= n <= 5000
+    -5000 <= nums[i] <= 5000
+    All the integers of nums are unique.
+    nums is sorted and rotated between 1 and n times.
 """
+from typing import List
 
 
-class Solution(object):
-    def findMin(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
         lo, hi = 0, len(nums)-1
         while lo < hi:
             mid = (lo+hi) // 2
@@ -47,6 +59,28 @@ print(sol.findMin([0, 1, 2, 3, 4, 5, 6]))
 print(sol.findMin([3, 4, 5, 6, 7, 0, 1, 2]))
 print(sol.findMin([1, 2]))
 print(sol.findMin([2, 1]))
+
+"""
+My another solution:
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        def binarySearch(lo, hi):
+            while lo <= hi:
+                mid = (lo + hi) // 2
+                nxt = (mid+1) % len(nums)
+                if nums[mid] > nums[nxt]:
+                    return nxt
+                elif nums[lo] <= nums[mid]:
+                    lo = mid + 1
+                else:
+                    hi = mid - 1
+
+            return nxt
+
+        index = binarySearch(0, len(nums)-1)
+        return nums[index]
+"""
 
 """
 Leetcode discuss solution:

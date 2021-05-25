1226cb7c1f9379e0c61599de87a36340425ab048
Sankeerth V S <sankeerth456@gmail.com>
2021-05-25 16:15:59 -0700

    Update set_matrix_zeros.py

--- Array/python/leetcode/set_matrix_zeros.py
+++ Array/python/leetcode/set_matrix_zeros.py
@@ -4,7 +4,6 @@
 Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in-place.
 
 Example 1:
-
 Input:
 [
   [1,1,1],
@@ -17,8 +16,8 @@ Output:
   [0,0,0],
   [1,0,1]
 ]
-Example 2:
 
+Example 2:
 Input:
 [
   [0,1,2,0],
@@ -31,7 +30,14 @@ Output:
   [0,4,5,0],
   [0,3,1,0]
 ]
+
+Constraints:
+    m == matrix.length
+    n == matrix[0].length
+    1 <= m, n <= 200
+    -231 <= matrix[i][j] <= 231 - 1
 """
+from typing import List
 
 
 class Solution:
@@ -41,26 +47,28 @@ class Solution:
     However, a conflict results in storing the presence of 0 for first row and first column, therefore have a
     variable for storing the value of the column.
     """
-    def setZeroes(self, matrix):
+    def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         :type matrix: List[List[int]]
         :rtype: void Do not return anything, modify matrix in-place instead.
         """
-        col0 = 1
-        for i in range(len(matrix)):
+        isCol0 = False
+        rows, cols = len(matrix), len(matrix[0])
+
+        for i in range(rows):
             if matrix[i][0] == 0:
-                col0 = 0
-            for j in range(1, len(matrix[0])):
+                isCol0 = True
+            for j in range(1, cols):
                 if matrix[i][j] == 0:
                     matrix[i][0] = 0
                     matrix[0][j] = 0
 
-        for i in range(len(matrix)-1, -1, -1):
-            for j in range(len(matrix[0])-1, 0, -1):
+        for i in range(rows-1, -1, -1):
+            for j in range(1, cols): # can do range(col-1, 0, -1) as well
                 if matrix[i][0] == 0 or matrix[0][j] == 0:
                     matrix[i][j] = 0
-
-            if col0 == 0:
+            
+            if isCol0:
                 matrix[i][0] = 0
 
         print(matrix)

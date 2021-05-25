517a3fb29034bf2a782bc337932fad8edc4e98b8
Sankeerth V S <sankeerth456@gmail.com>
2021-05-24 18:08:19 -0700

    Update largest_number.py

--- Array/python/leetcode/largest_number.py
+++ Array/python/leetcode/largest_number.py
@@ -4,14 +4,26 @@
 Given a list of non negative integers, arrange them such that they form the largest number.
 
 Example 1:
-
-Input: [10,2]
+Input: nums = [10,2]
 Output: "210"
-Example 2:
 
-Input: [3,30,34,5,9]
+Example 2:
+Input: nums = [3,30,34,5,9]
 Output: "9534330"
+
+Example 3:
+Input: nums = [1]
+Output: "1"
+
+Example 4:
+Input: nums = [10]
+Output: "10"
+
+Constraints:
+1 <= nums.length <= 100
+0 <= nums[i] <= 109
 """
+from typing import List
 
 
 class CustomCmp(object):
@@ -30,11 +42,7 @@ class CustomCmp(object):
 
 
 class Solution:
-    def largestNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: str
-        """
+    def largestNumber(self, nums: List[int]) -> str:
         sorted_ints = sorted(nums, key=CustomCmp, reverse=True)
         sorted_ints_to_strings = [str(num) for num in sorted_ints]
         return ''.join(sorted_ints_to_strings) if sorted_ints_to_strings[0] != "0" else "0"

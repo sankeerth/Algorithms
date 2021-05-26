6b998643fd3ba786d8c4e5e8cb475400954ff801
Sankeerth V S <sankeerth456@gmail.com>
2021-05-26 00:22:45 -0700

    Update merge_intervals.py
    56. Merge Intervals

--- Array/python/leetcode/merge_intervals.py
+++ Array/python/leetcode/merge_intervals.py
@@ -3,11 +3,49 @@
 
 Given a collection of intervals, merge all overlapping intervals.
 
-For example,
-Given [1,3],[2,6],[8,10],[15,18],
-return [1,6],[8,10],[15,18].
-"""
+Example 1:
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
 
+Example 2:
+Input: intervals = [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+
+Constraints:
+1 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= starti <= endi <= 104
+"""
+from typing import List
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        res = []
+        intervals.sort(key= lambda x: x[0])
+
+        res.append(intervals[0])
+        for curStart, curEnd in intervals[1:]:
+            prevStart, prevEnd = res[-1][0], res[-1][1]
+            if curStart <= prevEnd:
+                res[-1][1] = max(prevEnd, curEnd)
+            else:
+                res.append([curStart, curEnd])
+
+        return res
+
+
+sol = Solution()
+print(sol.merge([[1,3],[2,6],[8,10],[15,18]]))
+print(sol.merge([[1,3],[2,6],[8,10],[15,18],[10,12]]))
+print(sol.merge([[1,3],[2,6],[8,10],[15,18],[10,12],[9,13]]))
+print(sol.merge([[1,3],[2,6],[8,10],[15,18],[0,9]]))
+
+
+"""
+Older signature using Interval class
 
 class Interval(object):
     def __init__(self, s=0, e=0):
@@ -17,10 +55,10 @@ class Interval(object):
 
 class Solution(object):
     def merge(self, intervals):
-        """
+        '''
         :type intervals: List[Interval]
         :rtype: List[Interval]
-        """
+        '''
         merged_intervals = list()
         if not intervals:
             return merged_intervals
@@ -44,9 +82,9 @@ def create_intervals(list_of_intervals):
 
     return intervals
 
-
 sol = Solution()
 print(sol.merge(create_intervals([[1,3],[2,6],[8,10],[15,18]])))
 print(sol.merge(create_intervals([[1,3],[2,6],[8,10],[15,18],[10,12]])))
 print(sol.merge(create_intervals([[1,3],[2,6],[8,10],[15,18],[10,12],[9,13]])))
 print(sol.merge(create_intervals([[1,3],[2,6],[8,10],[15,18],[0,9]])))
+"""

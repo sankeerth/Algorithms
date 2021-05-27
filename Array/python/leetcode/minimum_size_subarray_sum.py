0fe81387947753b870070b48e74d3e8062f52035
Sankeerth V S <sankeerth456@gmail.com>
2021-05-26 17:11:10 -0700

    Update minimum_size_subarray_sum.py
    209. Minimum Size Subarray Sum

--- Array/python/leetcode/minimum_size_subarray_sum.py
+++ Array/python/leetcode/minimum_size_subarray_sum.py
@@ -4,41 +4,99 @@
 Given an array of n positive integers and a positive integer s, find the minimal length of a contiguous subarray
 of which the sum ≥ s. If there isn't one, return 0 instead.
 
-Example:
-
-Input: [2,3,1,2,4,3], s = 7
+Example 1:
+Input: target = 7, nums = [2,3,1,2,4,3]
 Output: 2
-Explanation: the subarray [4,3] has the minimal length under the problem constraint.
+Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+
+Example 2:
+Input: target = 4, nums = [1,4,4]
+Output: 1
+
+Example 3:
+Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+Output: 0
+
+Constraints:
+1 <= target <= 109
+1 <= nums.length <= 105
+1 <= nums[i] <= 105
 """
+from typing import List
 
 
 class Solution:
-    def minSubArrayLen(self, s, nums):
-        """
-        :type s: int
-        :type nums: List[int]
-        :rtype: int
-        """
-        if not nums:
-            return 0
-
-        i, j = 0, 0
-        result = float('inf')
-        current_sum = 0
+    def minSubArrayLen(self, s: int, nums: List[int]) -> int:
+        i, j, total, res = 0, 0, 0, float('inf')
+        if s == 1:
+            return 1
 
         while j < len(nums):
-            current_sum += nums[j]
-            while i <= j and current_sum >= s:
-                if j - i + 1 < result:
-                    result = j - i + 1
-                current_sum -= nums[i]
+            total += nums[j]
+            while total >= s and i <= j:
+                res = min(res, j-i+1)
+                total -= nums[i]
                 i += 1
             j += 1
 
-        return result if result != float('inf') else 0
+        return 0 if res == float('inf') else res
 
 
 sol = Solution()
 print(sol.minSubArrayLen(7, [2, 3, 1, 2, 4, 3]))
 print(sol.minSubArrayLen(0, [2, 3, 1, 2, 4, 3]))
 print(sol.minSubArrayLen(2, [1, 1, 3, 1, 1]))
+print(sol.minSubArrayLen(11, [1, 1, 3, 1, 1]))
+
+
+"""
+Leetcode O(n^2) solution for reference:
+
+int minSubArrayLen(int s, vector<int>& nums)
+{
+    int n = nums.size();
+    if (n == 0)
+        return 0;
+    int ans = INT_MAX;
+    vector<int> sums(n);
+    sums[0] = nums[0];
+    for (int i = 1; i < n; i++)
+        sums[i] = sums[i - 1] + nums[i];
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            int sum = sums[j] - sums[i] + nums[i];
+            if (sum >= s) {
+                ans = min(ans, (j - i + 1));
+                break; //Found the smallest subarray with sum>=s starting with index i, hence move to next index
+            }
+        }
+    }
+    return (ans != INT_MAX) ? ans : 0;
+}
+"""
+
+"""
+Leetcode O(N logN) solution to perform the inner for loop of above solution using binary search
+
+int minSubArrayLen(int s, vector<int>& nums)
+{
+    int n = nums.size();
+    if (n == 0)
+        return 0;
+    int ans = INT_MAX;
+    vector<int> sums(n + 1, 0); //size = n+1 for easier calculations
+    //sums[0]=0 : Meaning that it is the sum of first 0 elements
+    //sums[1]=A[0] : Sum of first 1 elements
+    //ans so on...
+    for (int i = 1; i <= n; i++)
+        sums[i] = sums[i - 1] + nums[i - 1];
+    for (int i = 1; i <= n; i++) {
+        int to_find = s + sums[i - 1];
+        auto bound = lower_bound(sums.begin(), sums.end(), to_find);
+        if (bound != sums.end()) {
+            ans = min(ans, static_cast<int>(bound - (sums.begin() + i - 1)));
+        }
+    }
+    return (ans != INT_MAX) ? ans : 0;
+}
+"""

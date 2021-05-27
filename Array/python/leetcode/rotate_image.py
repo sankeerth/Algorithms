0862753e1922939b57b5f324877c5ac1b001c4e8
Sankeerth V S <sankeerth456@gmail.com>
2021-05-27 01:00:29 -0700

    Update rotate_image.py
    48. Rotate Image

--- Array/python/leetcode/rotate_image.py
+++ Array/python/leetcode/rotate_image.py
@@ -10,22 +10,20 @@ Note:
 You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
 
 Example 1:
-
 Given input matrix =
 [
   [1,2,3],
   [4,5,6],
   [7,8,9]
 ],
-
 rotate the input matrix in-place such that it becomes:
 [
   [7,4,1],
   [8,5,2],
   [9,6,3]
 ]
-Example 2:
 
+Example 2:
 Given input matrix =
 [
   [ 5, 1, 9,11],
@@ -33,7 +31,6 @@ Given input matrix =
   [13, 3, 6, 7],
   [15,14,12,16]
 ],
-
 rotate the input matrix in-place such that it becomes:
 [
   [15,13, 2, 5],
@@ -41,14 +38,28 @@ rotate the input matrix in-place such that it becomes:
   [12, 6, 8, 9],
   [16, 7,10,11]
 ]
+
+Example 3:
+Input: matrix = [[1]]
+Output: [[1]]
+
+Example 4:
+Input: matrix = [[1,2],[3,4]]
+Output: [[3,1],[4,2]]
+
+Constraints:
+matrix.length == n
+matrix[i].length == n
+1 <= n <= 20
+-1000 <= matrix[i][j] <= 1000
 """
+from typing import List
 
 
-class Solution(object):
-    def rotate(self, matrix):
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
         """
-        :type matrix: List[List[int]]
-        :rtype: void Do not return anything, modify matrix in-place instead.
+        Do not return anything, modify matrix in-place instead.
         """
         i, j = 0, len(matrix)-1
         n = len(matrix)-1

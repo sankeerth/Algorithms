f3978df2a3c1f56f351cff3e29c290ee0547922a
Sankeerth V S <sankeerth456@gmail.com>
2021-05-26 17:56:06 -0700

    Create subarray_sums_divisible_by_k.py
    974. Subarray Sums Divisible by K

---
+++ Array/python/leetcode/subarray_sums_divisible_by_k.py
@@ -0,0 +1,59 @@
+"""
+974. Subarray Sums Divisible by K
+
+Given an array A of integers, return the number of (contiguous, non-empty) 
+subarrays that have a sum divisible by K.
+
+Example 1:
+Input: A = [4,5,0,-2,-3,1], K = 5
+Output: 7
+Explanation: There are 7 subarrays with a sum divisible by K = 5:
+[4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
+
+Note:
+1 <= A.length <= 30000
+-10000 <= A[i] <= 10000
+2 <= K <= 10000
+"""
+from typing import List
+
+
+"""
+Very good white-board explanation of the solution with the need to store and add frequencies of reminders.
+https://leetcode.com/problems/subarray-sums-divisible-by-k/discuss/413234/DETAILED-WHITEBOARD!-BEATS-100-(Do-you-really-want-to-understand-It)
+"""
+class Solution:
+    def subarraysDivByK(self, A: List[int], K: int) -> int:
+        res = 0
+        prefix = 0
+        count = [1] + [0] * K
+        for a in A:
+            prefix = (prefix + a) % K
+            res += count[prefix]
+            count[prefix] += 1
+        return res
+
+
+sol = Solution()
+print(sol.subarraysDivByK([4,5,0], 5))
+print(sol.subarraysDivByK([-2,-3,0], 5))
+print(sol.subarraysDivByK([4,5,0,-2,-3,1], 5))
+
+
+"""
+O(n^2) solution:
+
+class Solution:
+    def subarraysDivByK(self, A: List[int], K: int) -> int:
+        res = 0
+        for i in range(len(A)):
+            total = A[i]
+            if total % K == 0:
+                res += 1
+            for j in range(i+1, len(A)):
+                total += A[j]
+                if total % K == 0:
+                    res += 1
+
+        return res
+"""

8b4ae82a2c6ff4d7d3a6aea5464581d89f92cf6b
Sankeerth V S <sankeerth456@gmail.com>
2021-06-01 19:32:48 -0700

    Update sliding_window_maximum.py

--- Deque/python/leetcode/sliding_window_maximum.py
+++ Deque/python/leetcode/sliding_window_maximum.py
@@ -89,3 +89,33 @@ sol = Solution()
 print(sol.maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3))
 print(sol.maxSlidingWindow([3,3,5,5,6,7], 3))
 print(sol.maxSlidingWindow([9,6,11,8,10,5,4,13,93,14], 4))
+
+
+"""
+Using Deque:
+
+from collections import deque
+
+class Solution(object):
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        res, dq = [], deque()
+        if not nums:
+            return res
+
+        dq.append(0)        
+        for i in range(1, k):
+            while dq and nums[i] > nums[dq[-1]]:
+                dq.pop()
+            dq.append(i)
+        res.append(nums[dq[0]])
+
+        for i in range(k, len(nums)):
+            if dq[0] <= i - k:
+                dq.popleft()
+            while dq and nums[i] > nums[dq[-1]]:
+                dq.pop()
+            dq.append(i)
+            res.append(nums[dq[0]])
+
+        return res
+"""

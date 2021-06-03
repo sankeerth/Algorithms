eb5281d4145d272cc914ae1d51cfeba139b5f07e
Sankeerth V S <sankeerth456@gmail.com>
2021-06-02 18:09:33 -0700

    Update trapping_rain_water.py
    42. Trapping Rain Water

--- Array/python/leetcode/trapping_rain_water.py
+++ Array/python/leetcode/trapping_rain_water.py
@@ -1,44 +1,92 @@
 """
 42. Trapping Rain Water
 
-Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+Given n non-negative integers representing an elevation map where the width of each bar is 1, 
+compute how much water it is able to trap after raining.
 
-For example,
-Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
+Example 1:
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+
+Example 2:
+Input: height = [4,2,0,3,2,5]
+Output: 9
+
+Constraints:
+n == height.length
+0 <= n <= 3 * 104
+0 <= height[i] <= 105
 """
+from typing import List
 
-class Solution(object):
-    def trap(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
         if not height:
             return 0
+        
+        res = 0
+        maxFromLeft, maxFromRight = [0] * len(height), [0] * len(height)
+        maxFromLeft[0], maxFromRight[-1] = height[0], height[-1]
 
-        l = len(height)
-        max_height_to_left = [0] * l
-        max_height_to_right = [0] * l
+        for i in range(1, len(height)):
+            maxFromLeft[i] = max(maxFromLeft[i-1], height[i])
+        
+        for i in range(len(height)-2, -1, -1):
+            maxFromRight[i] = max(maxFromRight[i+1], height[i])
 
-        max_height_to_left[0] = height[0]
-        max_height_to_right[l - 1] = height[l - 1]
+        for i in range(len(height)):
+            res += min(maxFromLeft[i], maxFromRight[i]) - height[i]
 
-        for i in range(1, l):
-            max_height_to_left[i] = max(max_height_to_left[i - 1], height[i])
-            max_height_to_right[l - i - 1] = max(max_height_to_right[l - i], height[l - i - 1])
-
-        rain_water_trapped = 0
-        for i in range(l):
-            rain_water_trapped += min(max_height_to_left[i], max_height_to_right[i]) - height[i]
-
-        return rain_water_trapped
+        return res
 
 sol = Solution()
 print(sol.trap([0,1,0,2,1,0,1,3,2,1,2,1]))
 print(sol.trap([2,4,3,6,9,5]))
 
+
 """
-Leetcode discuss solution: O(n) time, O(1) space
+Leetcode solution using Stack:
+
+We keep a stack and iterate over the array. We add the index of the bar to the stack if bar is smaller than or equal to the bar at top of stack, 
+which means that the current bar is bounded by the previous bar in the stack. If we found a bar longer than that at the top, 
+we are sure that the bar at the top of the stack is bounded by the current bar and a previous bar in the stack, hence, we can pop it and 
+add resulting trapped water to ans.
+
+Algorithm:
+Use stack to store the indices of the bars.
+
+Iterate the array:
+While stack is not empty and height[current]>height[st.top()]
+It means that the stack element can be popped. Pop the top element as top.
+Find the distance between the current element and the element at top of stack, which is to be filled. distance=current−st.top()−1
+Find the bounded height bounded_height=min(height[current],height[st.top()])−height[top]
+Add resulting trapped water to answer ans+=distance×bounded_height
+Push current index to top of the stack
+Move current to the next position
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        res = 0
+        stack = []
+
+        for i in range(len(height)):
+            while stack and height[i] > height[stack[-1]]:
+                top = stack.pop()
+                if stack:
+                    w = i - stack[-1] - 1
+                    h = min(height[i], height[stack[-1]]) - height[top]
+                    res += w * h
+
+            stack.append(i)
+
+        return res
+"""
+
+"""
+Leetcode solution: O(n) time, O(1) space
+Similar to the solution on top with maxFromLeft and maxFromRight using one variable instead of an array
 
 instead of calculating area by height*width, we can think it in a cumulative way.
 In other words, sum water amount of each bin(width=1).
@@ -46,25 +94,20 @@ Search from left to right and maintain a max height of left and right separately
 Fix the higher one and flow water from the lower part.
 For example, if current height of left is lower, we fill water in the left bin. Until left meets right, we filled the whole container.
 
-class Solution {
-public:
-    int trap(int A[], int n) {
-        int left=0; int right=n-1;
-        int res=0;
-        int maxleft=0, maxright=0;
-        while(left<=right){
-            if(A[left]<=A[right]){
-                if(A[left]>=maxleft) maxleft=A[left];
-                else res+=maxleft-A[left];
-                left++;
-            }
-            else{
-                if(A[right]>=maxright) maxright= A[right];
-                else res+=maxright-A[right];
-                right--;
-            }
-        }
-        return res;
-    }
-};
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        res, left, right = 0, 0, len(height)-1
+        leftMax, rightMax = 0, 0
+
+        while left < right:
+            if height[left] < height[right]:
+                leftMax = max(leftMax, height[left])
+                res += leftMax - height[left]
+                left += 1
+            else:
+                rightMax = max(rightMax, height[right])
+                res += rightMax - height[right]
+                right -= 1
+
+        return res
 """

c007f4e9a7e49d08632c6a688461c16ff4ccee20
Sankeerth V S <sankeerth456@gmail.com>
2021-06-07 20:05:23 -0700

    Update unique_paths_2.py

--- DynamicProgramming/python/leetcode/unique_paths_2.py
+++ DynamicProgramming/python/leetcode/unique_paths_2.py
@@ -8,56 +8,49 @@ The robot is trying to reach the bottom-right corner of the grid (marked 'Finish
 Now consider if some obstacles are added to the grids. How many unique paths would there be?
 An obstacle and empty space is marked as 1 and 0 respectively in the grid.
 
-For example,
-There is one obstacle in the middle of a 3x3 grid as illustrated below.
+Example 1:
+Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+Output: 2
+Explanation: There is one obstacle in the middle of the 3x3 grid above.
+There are two ways to reach the bottom-right corner:
+1. Right -> Right -> Down -> Down
+2. Down -> Down -> Right -> Right
 
-[
-  [0,0,0],
-  [0,1,0],
-  [0,0,0]
-]
-The total number of unique paths is 2
+Example 2:
+Input: obstacleGrid = [[0,1],[0,0]]
+Output: 1
+
+Constraints:
+m == obstacleGrid.length
+n == obstacleGrid[i].length
+1 <= m, n <= 100
+obstacleGrid[i][j] is 0 or 1.
 """
+from typing import List
 
 
-class Solution(object):
-    def uniquePathsWithObstacles(self, obstacleGrid):
-        """
-        :type obstacleGrid: List[List[int]]
-        :rtype: int
-        """
-        m = len(obstacleGrid)
-        n = len(obstacleGrid[0])
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        rows, cols = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [[0] * cols for _ in range(rows)]
 
-        if m == 0 or n == 0:
+        if obstacleGrid[0][0] == 1:
             return 0
+        
+        dp[0][0] = 1
 
-        grid = list()
-        for i in range(m):
-            grid.append(list([1] * n))
+        for i in range(1, rows):
+            dp[i][0] = 0 if obstacleGrid[i][0] == 1 else dp[i-1][0]
+        
+        for i in range(1, cols):
+            dp[0][i] = 0 if obstacleGrid[0][i] == 1 else dp[0][i-1]
 
-        grid[0][0] = 0 if obstacleGrid[0][0] else 1
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if obstacleGrid[i][j] != 1:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
 
-        for i in range(1, m):
-            if grid[i - 1][0] and not obstacleGrid[i][0]:
-                grid[i][0] = 1
-            else:
-                grid[i][0] = 0
-
-        for i in range(1, n):
-            if grid[0][i - 1] and not obstacleGrid[0][i]:
-                grid[0][i] = 1
-            else:
-                grid[0][i] = 0
-
-        for i in range(1, m):
-            for j in range(1, n):
-                if obstacleGrid[i][j] == 1:
-                    grid[i][j] = 0
-                else:
-                    grid[i][j] = grid[i - 1][j] + grid[i][j - 1]
-
-        return grid[m - 1][n - 1]
+        return dp[rows-1][cols-1]
 
 
 sol = Solution()
@@ -65,3 +58,42 @@ print(sol.uniquePathsWithObstacles([[0,0,0],[0,1,0],[0,0,0]]))
 print(sol.uniquePathsWithObstacles([[1,0,0],[0,1,0],[0,0,0]]))
 print(sol.uniquePathsWithObstacles([[0,1,0],[0,1,0],[0,0,0]]))
 print(sol.uniquePathsWithObstacles([[0,0,1],[0,0,0],[0,0,0]]))
+
+
+"""
+Leetcode discuss solution modify the obstable grid itself to get the final result:
+
+class Solution(object):
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+
+        # If the starting cell has an obstacle, then simply return as there would be
+        # no paths to the destination.
+        if obstacleGrid[0][0] == 1:
+            return 0
+
+        # Number of ways of reaching the starting cell = 1.
+        obstacleGrid[0][0] = 1
+
+        # Filling the values for the first column
+        for i in range(1,m):
+            obstacleGrid[i][0] = int(obstacleGrid[i][0] == 0 and obstacleGrid[i-1][0] == 1)
+
+        # Filling the values for the first row        
+        for j in range(1, n):
+            obstacleGrid[0][j] = int(obstacleGrid[0][j] == 0 and obstacleGrid[0][j-1] == 1)
+
+        # Starting from cell(1,1) fill up the values
+        # No. of ways of reaching cell[i][j] = cell[i - 1][j] + cell[i][j - 1]
+        # i.e. From above and left.
+        for i in range(1,m):
+            for j in range(1,n):
+                if obstacleGrid[i][j] == 0:
+                    obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1]
+                else:
+                    obstacleGrid[i][j] = 0
+
+        # Return value stored in rightmost bottommost cell. That is the destination.            
+        return obstacleGrid[m-1][n-1]
+"""

1f9f3e8f5f0a437ca1a0ac8ac6ba3da9dbe2ea82
Sankeerth V S <sankeerth456@gmail.com>
2021-06-07 19:48:07 -0700

    Update unique_paths.py
    62. Unique Paths

--- DynamicProgramming/python/leetcode/unique_paths.py
+++ DynamicProgramming/python/leetcode/unique_paths.py
@@ -4,30 +4,47 @@
 A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
 The robot can only move either down or right at any point in time.
 The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
-
 How many possible unique paths are there?
+
+Example 1:
+Input: m = 3, n = 7
+Output: 28
+
+Example 2:
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Down -> Down
+2. Down -> Down -> Right
+3. Down -> Right -> Down
+
+Example 3:
+Input: m = 7, n = 3
+Output: 28
+
+Example 4:
+Input: m = 3, n = 3
+Output: 6
+
+Constraints:
+1 <= m, n <= 100
+It's guaranteed that the answer will be less than or equal to 2 * 109.
 """
 
 
-class Solution(object):
-    def uniquePaths(self, m, n):
-        """
-        :type m: int
-        :type n: int
-        :rtype: int
-        """
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
         if m == 0 or n == 0:
             return 0
 
-        grid = list()
-        for i in range(m):
-            grid.append(list([1] * n))
+        dp = [[1] * n for _ in range(m)]
 
         for i in range(1, m):
             for j in range(1, n):
-                grid[i][j] = grid[i - 1][j] + grid[i][j - 1]
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
 
-        return grid[m - 1][n - 1]
+        return dp[m - 1][n - 1]
 
 
 sol = Solution()
@@ -36,3 +53,20 @@ print(sol.uniquePaths(1,2))
 print(sol.uniquePaths(3,5))
 print(sol.uniquePaths(7,3))
 print(sol.uniquePaths(12,23))
+
+"""
+Another variant of DP: answer at [0][0]
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        if m == 0 or n == 0:
+            return 0
+
+        dp = [[1] * n for _ in range(m)]
+
+        for i in range(m-2, -1, -1):
+            for j in range(n-2, -1, -1):
+                dp[i][j] = dp[i+1][j] + dp[i][j+1]
+
+        return dp[0][0]
+"""

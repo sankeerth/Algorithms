459b1b22e812d19545acf7a10a8888b3e258fffb
Sankeerth V S <sankeerth456@gmail.com>
2021-06-21 19:40:09 -0700

    Update count_smaller_numbers_after_self.py

--- DivideAndConquer/python/leetcode/count_smaller_numbers_after_self.py
+++ DivideAndConquer/python/leetcode/count_smaller_numbers_after_self.py
@@ -1,76 +1,78 @@
 """
 315. Count of Smaller Numbers After Self
 
+You are given an integer array nums and you have to return a new counts array. 
+The counts array has the property where counts[i] is the number of smaller elements to the right of nums[i].
 
-You are given an integer array nums and you have to return a new counts array. The counts array has the property where counts[i] is the number of smaller elements to the right of nums[i].
-
-Example:
-
-Input: [5,2,6,1]
+Example 1:
+Input: nums = [5,2,6,1]
 Output: [2,1,1,0]
 Explanation:
 To the right of 5 there are 2 smaller elements (2 and 1).
 To the right of 2 there is only 1 smaller element (1).
 To the right of 6 there is 1 smaller element (1).
 To the right of 1 there is 0 smaller element.
+
+Example 2:
+Input: nums = [-1]
+Output: [0]
+
+Example 3:
+Input: nums = [-1,-1]
+Output: [0,0]
+
+Constraints:
+    1 <= nums.length <= 105
+    -104 <= nums[i] <= 104
 """
+from typing import List
 
 
 class Solution:
-    def countSmaller(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        result = [0] * len(nums)
-        indices = [i for i in range(len(nums))]
-        if not nums:
-            return result
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        res = [0] * len(nums)
+        nums = [(num, i) for i, num in enumerate(nums)]
 
-        def merge_sort(lo, hi):
-            if lo >= hi:
+        def countSmallerRecursive(l, r):
+            if l+1 == r:
                 return
-            mid = int(lo + (hi - lo)/2)
-            merge_sort(lo, mid)
-            merge_sort(mid+1, hi)
-            merge(lo, hi, mid)
+            
+            mid = (l + r) // 2
+            countSmallerRecursive(l, mid)
+            countSmallerRecursive(mid, r)
 
-        def merge(lo, hi, mid):
-            i, j, k, count = lo, mid+1, 0, 0
-            temp = [0] * (hi-lo+1)
-            index = [0] * (hi-lo+1)
+            i, j = l, mid
+            while i < mid:
+                while j < r and nums[j][0] < nums[i][0]:
+                    j += 1
+                res[nums[i][1]] += j - mid
+                i += 1
 
-            while i <= mid and j <= hi:
-                if nums[i] <= nums[j]:
+            temp = [0] * (r-l)
+            i, j, k = l, mid, 0
+            while i < mid and j < r:
+                if nums[i][0] <= nums[j][0]:
                     temp[k] = nums[i]
-                    result[indices[i]] += count
-                    index[k] = indices[i]
                     i += 1
                 else:
                     temp[k] = nums[j]
-                    count += 1
-                    index[k] = indices[j]
                     j += 1
                 k += 1
-
-            while i <= mid:
+            
+            while i < mid:
                 temp[k] = nums[i]
-                result[indices[i]] += count
-                index[k] = indices[i]
                 i += 1
                 k += 1
-
-            while j <= hi:
+            
+            while j < r:
                 temp[k] = nums[j]
-                index[k] = indices[j]
                 j += 1
                 k += 1
-
-            nums[lo:hi+1] = temp[:]
-            indices[lo:hi+1] = index[:]
-
-        merge_sort(0, len(nums)-1)
-        return result
+            
+            nums[l:r] = temp
+        
+        countSmallerRecursive(0, len(nums))
+        return res
 
 
 sol = Solution()
@@ -84,35 +86,31 @@ print(sol.countSmaller([2, 2, 2, 2]))
 print(sol.countSmaller([1, 2, 3, 4]))
 
 
-'''
+"""
 My simpler solution:
 
 class Solution:
-    def countSmaller(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        result = [0] * len(nums)
-        nums = list(enumerate(nums))
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        res = [0] * len(nums)
+        nums = [(num, i) for i, num in enumerate(nums)]
 
-        def count_smaller_recr(lo, hi):
-            if lo >= hi:
+        def countSmallerRecursive(l, r):
+            if l+1 == r:
                 return
+            
+            mid = (l + r) // 2
+            countSmallerRecursive(l, mid)
+            countSmallerRecursive(mid, r)
 
-            mid = (lo + hi) // 2
-            count_smaller_recr(lo, mid)
-            count_smaller_recr(mid+1, hi)
-
-            i, j = lo, mid+1
-            while i <= mid:
-                while j <= hi and nums[i][1] > nums[j][1]:
+            i, j = l, mid
+            while i < mid:
+                while j < r and nums[j][0] < nums[i][0]:
                     j += 1
-                result[nums[i][0]] += j - mid - 1
+                res[nums[i][1]] += j - mid
                 i += 1
-            nums[lo: hi+1] = sorted(nums[lo:hi+1], key=lambda x: x[1])
 
-        count_smaller_recr(0, len(nums)-1)
-
-        return result
-'''
+            nums[l:r] = sorted(nums[l:r], key=lambda x: x[0]) # instead of custom merge
+        
+        countSmallerRecursive(0, len(nums))
+        return res
+"""

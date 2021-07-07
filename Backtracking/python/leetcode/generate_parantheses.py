9848e2f71593cb0b8ab944728019168e4ed8d2d7
Sankeerth V S <sankeerth456@gmail.com>
2021-07-06 19:41:59 -0700

    Update generate_parantheses.py

--- Backtracking/python/leetcode/generate_parantheses.py
+++ Backtracking/python/leetcode/generate_parantheses.py
@@ -1,83 +1,81 @@
 """
-    22. Generate Parentheses
+22. Generate Parentheses
 
 Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
-For example, given n = 3, a solution set is:
 
-[
-  "((()))",
-  "(()())",
-  "(())()",
-  "()(())",
-  "()()()"
-]
+Example 1:
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+Example 2:
+Input: n = 1
+Output: ["()"]
+
+Constraints:
+1 <= n <= 8
 """
+from typing import List
+
 
 class Solution(object):
-    def generateParenthesis(self, n):
+    def generateParenthesis(self, n: int) -> List[str]:
         """
         :type n: int
         :rtype: List[str]
         """
-        if n == 0:
-            return [""]
+        res = []
+        def generateParenthesisRecursive(o, c, cur):
+            if o == n:
+                cur += ')' * (n-c)
+                res.append(cur)
+                return
+            
+            generateParenthesisRecursive(o+1, c, cur + '(')
+            for i in range(c, o):
+                cur += ')'
+                generateParenthesisRecursive(o+1, i+1, cur + '(')
 
-        result = list()
+        generateParenthesisRecursive(0, 0, "")
+        return res
 
-        def generate_parantheses_recr(open, close, s):
-            if open == 0:
-                for i in range(close):
-                    s += ')'
-                result.append(s)
-            else:
-                generate_parantheses_recr(open-1, close, s+'(')
-                if open < close:
-                    generate_parantheses_recr(open, close-1, s+')')
-
-        generate_parantheses_recr(n-1, n, '(')
-
-        return result
 
 sol = Solution()
 print(sol.generateParenthesis(3))
 print(sol.generateParenthesis(0))
 print(sol.generateParenthesis(4))
 
+
 """
-Inefficient C++ code using only recursion
+Leetcode discuss solution:
 
-class Solution {
-public:
-    vector<string> generateParenthesis(int n) {
-        return generateParanthesis(n,n);
-    }
+class Solution(object):
+    def generateParenthesis(self, n: int) -> List[str]:
+        if n == 0:
+            return [""]
 
-    vector <string> generateParanthesis(int start, int close){
-        vector<string> returnValue;
-        if(start == 0){
-            string str = "";
-            for(int i = 0; i < close; i++)
-                str+= ')';
-            returnValue.push_back(str);
-            return returnValue;
-        }
-        if(start == close){
-            vector<string> ret1 = generateParanthesis(start - 1, close);
-            for(string s : ret1){
-                returnValue.push_back("(" + s);
-            }
-        }
-        else{
-            vector<string> ret1 = generateParanthesis(start - 1, close);
-            for(string s : ret1){
-                returnValue.push_back("(" + s);
-            }
-            vector<string> ret2 = generateParanthesis(start, close - 1);
-            for(string s : ret2){
-                returnValue.push_back(")" + s);
-            }
-        }
-        return returnValue;
-    }
-};
+        res = []
+        def generateParenthesisRecursive(open, close, s):
+            if open == 0:
+                for i in range(close):
+                    s += ')'
+                res.append(s)
+            else:
+                generateParenthesisRecursive(open-1, close, s+'(')
+                if open < close:
+                    generateParenthesisRecursive(open, close-1, s+')')
+
+        generateParenthesisRecursive(n-1, n, '(')
+        return res
+"""
+
+"""
+Complexity Analysis
+
+Our complexity analysis rests on understanding how many elements there are in generateParenthesis(n). 
+This analysis is outside the scope of this article, but it turns out this is the n-th Catalan number 1/{n+1} binom{2n}{n} 
+which is bounded asymptotically by {4^n}/{n*sqrt{n}} 
+
+Time Complexity : O({4^n}/{sqrt{n}}). Each valid sequence has at most n steps during the backtracking procedure.
+
+Space Complexity : O({4^n}/{sqrt{n}}), as described above, and using O(n)O(n) space to store the sequence.
 """

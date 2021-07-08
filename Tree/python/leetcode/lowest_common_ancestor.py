76be0740691bf59308da57065041b1356b9b5cd5
Sankeerth V S <sankeerth456@gmail.com>
2021-07-07 17:15:14 -0700

    Update lowest_common_ancestor.py

--- Tree/python/leetcode/lowest_common_ancestor.py
+++ Tree/python/leetcode/lowest_common_ancestor.py
@@ -15,26 +15,36 @@ lowest node in T that has both v and w as descendants (where we allow a node to 
          7   4
 For example, the lowest common ancestor (LCA) of nodes 5 and 1 is 3. Another example is LCA of nodes 5 and 4 is 5,
 since a node can be a descendant of itself according to the LCA definition.
+
+Example 2:
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+Output: 5
+Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+
+Example 3:
+Input: root = [1,2], p = 1, q = 2
+Output: 1
+
+Constraints:
+The number of nodes in the tree is in the range [2, 105].
+-109 <= Node.val <= 109
+All Node.val are unique.
+p != q
+p and q will exist in the tree.
 """
-
-
 from Tree.python.common.tree_operations import deserialize, TreeNode, print_levelorder_leetcode_style
 
 
-class Solution(object):
-    def lowestCommonAncestor(self, root, p, q):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: TreeNode
-        """
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         if root in (None, p, q):
             return root
+
         left = self.lowestCommonAncestor(root.left, p, q)
         right = self.lowestCommonAncestor(root.right, p, q)
         if left and right:
             return root
+
         return left if left is not None else right
 
 

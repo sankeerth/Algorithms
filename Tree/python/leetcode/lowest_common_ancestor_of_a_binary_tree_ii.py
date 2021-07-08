c570297f01dea275239071a9035e8825cdf21637
Sankeerth V S <sankeerth456@gmail.com>
2021-07-07 17:38:53 -0700

    Create lowest_common_ancestor_of_a_binary_tree_ii.py

---
+++ Tree/python/leetcode/lowest_common_ancestor_of_a_binary_tree_ii.py
@@ -0,0 +1,68 @@
+"""
+1644. Lowest Common Ancestor of a Binary Tree II
+
+Given the root of a binary tree, return the lowest common ancestor (LCA) of two given nodes, p and q. If either node p or q does not exist in the tree, return null. 
+All values of the nodes in the tree are unique.
+According to the definition of LCA on Wikipedia: "The lowest common ancestor of two nodes p and q in a binary tree T is the lowest node that has both p and q as descendants 
+(where we allow a node to be a descendant of itself)". A descendant of a node x is a node y that is on the path from node x to some leaf node.
+
+Example 1:
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.
+
+Example 2:
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+Output: 5
+Explanation: The LCA of nodes 5 and 4 is 5. A node can be a descendant of itself according to the definition of LCA.
+
+Example 3:
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 10
+Output: null
+Explanation: Node 10 does not exist in the tree, so return null.
+ 
+Constraints:
+The number of nodes in the tree is in the range [1, 104].
+-109 <= Node.val <= 109
+All Node.val are unique.
+p != q
+"""
+from Tree.python.common.tree_operations import deserialize, TreeNode, print_levelorder_leetcode_style
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        visited = set()
+        
+        def lowestCommonAncestorRecursive(root):
+            if not root:
+                return None
+            
+            if root is p or root is q:
+                visited.add(root)
+                if len(visited) == 2:
+                    return root
+            
+            left = lowestCommonAncestorRecursive(root.left)
+            right = lowestCommonAncestorRecursive(root.right)
+            
+            
+            if root is p or root is q:
+                return root
+            
+            if left and right:
+                return root
+            
+            return left if left else right
+        
+        lca = lowestCommonAncestorRecursive(root)
+        if len(visited) == 2:
+            return lca
+        return None
+
+
+sol = Solution()
+r = deserialize('[3,5,1,6,2,0,8,null,null,7,4]')
+print_levelorder_leetcode_style(sol.lowestCommonAncestor(r, r.left, r.left.right.right))
+print_levelorder_leetcode_style(sol.lowestCommonAncestor(r, r.left.left, r.left.right))
+print_levelorder_leetcode_style(sol.lowestCommonAncestor(r, r.left.left.left, None))

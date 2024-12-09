346270bf57ed21631e98dba47a9cd9d0c7c93329
Sankeerth V S <sankeerth456@gmail.com>
2024-12-09 13:55:19 -0800

    Update all_possible_full_binary_trees.py

--- Tree/python/leetcode/all_possible_full_binary_trees.py
+++ Tree/python/leetcode/all_possible_full_binary_trees.py
@@ -1,38 +1,41 @@
 """
+894. All Possible Full Binary Trees
+
+Given an integer n, return a list of all possible full binary trees with n nodes. Each node of each tree in the answer must have Node.val == 0.
+Each element of the answer is the root node of one possible tree. You may return the final list of trees in any order.
 A full binary tree is a binary tree where each node has exactly 0 or 2 children.
-Return a list of all possible full binary trees with N nodes. 
-Each element of the answer is the root node of one possible tree.
-Each node of each tree in the answer must have node.val = 0. You may return the final list of trees in any order.
 
 Example 1:
-Input: 7
+Input: n = 7
 Output: [[0,0,0,null,null,0,0,null,null,0,0],[0,0,0,null,null,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,null,null,null,null,0,0],[0,0,0,0,0,null,null,0,0]]
-Explanation:
 
-Note:
-1 <= N <= 20
+Example 2:
+Input: n = 3
+Output: [[0,0,0]]
+
+Constraints:
+1 <= n <= 20
 """
 from typing import List
 from Tree.python.common.tree_operations import print_trees
 
 
 class Solution:
-    def allPossibleFBT(self, N: int) -> List[TreeNode]:
-        if N % 2 == 0:
+    def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
+        if n % 2 == 0:
             return []
+        
+        memoized = {1: [TreeNode(0)]}
 
-        memoizedFBTs = {}
-        memoizedFBTs[1] = [TreeNode(0)]
-
-        def allPossibleFBTRecursive(N):
-            if N in memoizedFBTs:
-                return memoizedFBTs[N]
-
+        def allPossibleFBTRec(n):
+            if n in memoized:
+                return memoized[n]
+            
             nodes = []
-            for i in range(2, N, 2):
-                left = allPossibleFBTRecursive(i-1)
-                right = allPossibleFBTRecursive(N-i)
-                
+            for i in range(2, n, 2):
+                left = allPossibleFBTRec(i-1)
+                right = allPossibleFBTRec(n-i)
+            
                 for lNode in left:
                     for rNode in right:
                         node = TreeNode(0)
@@ -40,10 +43,10 @@ class Solution:
                         node.right = rNode
                         nodes.append(node)
             
-            memoizedFBTs[N] = nodes
+            memoized[n] = nodes
             return nodes
-
-        return allPossibleFBTRecursive(N)
+        
+        return allPossibleFBTRec(n)
 
 
 s = Solution()

7b0733bf860210f8b8e7a6cb697cd290d76ff6a3
Sankeerth V S <sankeerth456@gmail.com>
2024-12-25 23:52:11 -0800

    Update remove_duplicates_from_sorted_array.py

--- Array/python/leetcode/remove_duplicates_from_sorted_array.py
+++ Array/python/leetcode/remove_duplicates_from_sorted_array.py
@@ -24,18 +24,15 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-
         i, j = 1, 1
-        while i < len(nums):
-            if nums[i] != nums[i-1]:
-                nums[j] = nums[i]
-                j += 1
-            i += 1
+
+        while j < len(nums):
+            if nums[j] != nums[i-1]:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+            j += 1
         
-        print(nums)
-        return j
+        return i
 
 
 sol = Solution()

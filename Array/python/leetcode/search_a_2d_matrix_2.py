0d4b0b8740e0b3a1cd97b45078e0ff73b84d226e
Sankeerth V S <sankeerth456@gmail.com>
2024-12-25 16:37:10 -0800

    Update search_a_2d_matrix_2.py

--- Array/python/leetcode/search_a_2d_matrix_2.py
+++ Array/python/leetcode/search_a_2d_matrix_2.py
@@ -42,25 +42,17 @@ class Solution:
     """
 
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
-        if not matrix:
-            return False
+        rows, cols = len(matrix), len(matrix[0])
+        i, j = 0, cols-1
 
-        m, n = len(matrix)-1, len(matrix[0])-1
-        i, j = 0, n
-
-        while i <= m and j >= 0:
+        while i < rows and j >= 0:
             if matrix[i][j] == target:
                 return True
             elif matrix[i][j] > target:
                 j -= 1
             else:
                 i += 1
-
+        
         return False
 
 

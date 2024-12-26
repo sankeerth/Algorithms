56cc28ea7d669545e3daaf3c61aca0aa71801c81
Sankeerth V S <sankeerth456@gmail.com>
2024-12-25 16:02:36 -0800

    Update rotate_image.py

--- Array/python/leetcode/rotate_image.py
+++ Array/python/leetcode/rotate_image.py
@@ -150,24 +150,43 @@ Then flip the matrix horizontally. (swap(matrix[i][j], matrix[i][matrix.length-1
 8  5  2
 9  6  3
 
-public class Solution {
-    public void rotate(int[][] matrix) {
-        for(int i = 0; i<matrix.length; i++){
-            for(int j = i; j<matrix[0].length; j++){
-                int temp = 0;
-                temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
-            }
-        }
-        for(int i =0 ; i<matrix.length; i++){
-            for(int j = 0; j<matrix.length/2; j++){
-                int temp = 0;
-                temp = matrix[i][j];
-                matrix[i][j] = matrix[i][matrix.length-1-j];
-                matrix[i][matrix.length-1-j] = temp;
-            }
-        }
-    }
-}
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+
+        def transpose():
+            for i in range(n):
+                for j in range(i, n):
+                    matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        def flipHorizontal():
+            for i in range(n):
+                j, k = 0, n-1
+                while j < k:
+                    matrix[i][j], matrix[i][k] = matrix[i][k], matrix[i][j]
+                    j += 1
+                    k -= 1
+
+        transpose()
+        flipHorizontal()
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+
+        def transpose():
+            for i in range(n):
+                for j in range(i, n):
+                    matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        
+        def flipVertical():
+            for j in range(n):
+                i, k = 0, n-1
+                while i < k:
+                    matrix[i][j], matrix[k][j] = matrix[k][j], matrix[i][j]
+                    i += 1
+                    k -= 1
+
+        flipVertical()
+        transpose()
 """

1a48ebe1869f2abd25c1790a1501015b30032fbe
Sankeerth V S <sankeerth456@gmail.com>
2025-01-04 17:39:28 -0800

    Update word_ladder.py

--- BFS/python/leetcode/word_ladder.py
+++ BFS/python/leetcode/word_ladder.py
@@ -106,63 +106,42 @@ sol = Solution()
 print(sol.ladderLength("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
 
 
-'''
+"""
 Leetcode discuss solution:
 
-class Solution(object):
-    def ladderLength(self, beginWord, endWord, wordList):
-                
-        def make_p2w(word_list, w_idxs):
-            """Creates a map of all combinations of words with missing letters mapped 
-            to all words in the list that match that pattern.
-            E.g. hot -> {'_ot': ['hot'], 'h_t': ['hot'], 'ho_': ['hot']}
-            """
-            p2w = collections.defaultdict(list)
-            
-            for word in word_list:
-                for i, j in w_idxs:
-                    p = word[:i] + "_" + word[j:]
-                    p2w[p].append(word)
-            return p2w
-            
-        def bfs_words(begin, end, w_idxs, p2w):
-            queue = collections.deque([(begin, 1)])
-            visited = set([begin])
-                        
-            while queue:
-                # Get the next node to explore from the top of the queue
-                word, depth = queue.popleft()
-                
-                # Get the node's children 
-                # By recreating all possible patterns for that string
-                for i,j in w_idxs:
-                    p = word[:i] + "_" + word[j:]
-                    neighbor_words = p2w[p]
-                    # Iterate through children
-                    for nw in neighbor_words:
-                        if nw not in visited:
-                            # Goal check (before adding to the queue)
-                            if nw == end:
-                                return depth+1
-                            # Add to visited
-                            # These is no reason to wait to mark nodes as visited. Because this is 
-                            # a BFS, once a node has been seen that is the earliest it could have
-                            # possibly been seen so any other path to that node would either be 
-                            # longer or the same length as what we've already observed.
-                            visited.add(nw)                            
-                            # Add to the end of the queue
-                            queue.append((nw, depth+1))
-            return 0
-        
-        # Get word length and character indexes
-        wl = len(beginWord)
-        w_indexes = zip(range(wl), range(1, wl+1))
-        # Preprocess words into a map
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        seen = {beginWord}
+        queue = [(beginWord, 1)]
+        wildcardMap = defaultdict(list)
         wordList.append(beginWord)
-        patterns2words = make_p2w(wordList, w_indexes)
-        # Do the search
-        return bfs_words(beginWord, endWord, w_indexes, patterns2words)
-'''
+
+        def buildWildcardMap():
+            for word in wordList:
+                for i in range(len(word)):
+                    wildcardWord = word[:i] + '*' + word[i+1:]
+                    wildcardMap[wildcardWord].append(word)
+        
+        buildWildcardMap()
+
+        while queue:
+            word1, count = queue.pop(0)
+            if word1 == endWord:
+                return count
+            
+            for i in range(len(word1)):
+                wildcardWord = word1[:i] + '*' + word1[i+1:]
+                words = wildcardMap[wildcardWord]
+                for word2 in words:
+                    if word2 not in seen:
+                        if word2 == endWord:
+                            return count+1
+                        queue.append((word2, count+1))
+                        seen.add(word2)
+                        
+        return 0
+
+"""
 
 
 """

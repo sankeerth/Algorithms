5f4028586296a458318f05d916007c00276b6624
Sankeerth V S <sankeerth456@gmail.com>
2025-01-04 21:09:28 -0800

    Update sliding_window_maximum.py

--- Deque/python/leetcode/sliding_window_maximum.py
+++ Deque/python/leetcode/sliding_window_maximum.py
@@ -89,33 +89,28 @@ sol = Solution()
 print(sol.maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3))
 print(sol.maxSlidingWindow([3,3,5,5,6,7], 3))
 print(sol.maxSlidingWindow([9,6,11,8,10,5,4,13,93,14], 4))
-
+print(sol.maxSlidingWindow([1,3,1,2,0,5], 3))
+print(sol.maxSlidingWindow([9,10,9,-7,-4,-8,2,-6], 3))
 
 """
 Using Deque:
 
 from collections import deque
 
-class Solution(object):
+
+class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        res, dq = [], deque()
-        if not nums:
-            return res
+        res = []
+        dq = deque()
 
-        dq.append(0)        
-        for i in range(1, k):
-            while dq and nums[i] > nums[dq[-1]]:
-                dq.pop()
-            dq.append(i)
-        res.append(nums[dq[0]])
-
-        for i in range(k, len(nums)):
-            if dq[0] <= i - k:
+        for i in range(len(nums)):
+            while dq and dq[0] <= i-k:
                 dq.popleft()
             while dq and nums[i] > nums[dq[-1]]:
                 dq.pop()
             dq.append(i)
             res.append(nums[dq[0]])
 
-        return res
+        return res[k-1:]
+        
 """

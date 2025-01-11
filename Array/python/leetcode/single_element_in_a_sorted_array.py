40e24dd5129749a0b448bc8b9998dc980d5ff9b9
Sankeerth V S <sankeerth456@gmail.com>
2025-01-11 15:53:17 -0800

    Update single_element_in_a_sorted_array.py

--- Array/python/leetcode/single_element_in_a_sorted_array.py
+++ Array/python/leetcode/single_element_in_a_sorted_array.py
@@ -44,3 +44,35 @@ print(sol.singleNonDuplicate([3,3,7,7,10,11,11]))
 print(sol.singleNonDuplicate([1,2,2]))
 print(sol.singleNonDuplicate([1,1,2]))
 print(sol.singleNonDuplicate([2,3,3,7,7,11,11]))
+
+
+"""
+Leetcode solution:
+
+'''
+We need to make sure our mid index is even. We can do this by dividing lo and hi in the usual way, but then decrementing it by 1 if it is odd. 
+This also ensures that if we have an even number of even indexes to search, that we are getting the lower middle (incrementing by 1 here would not work, 
+it'd lead to an infinite loop as the search space would not be reduced in some cases).
+
+Then we check whether or not the mid index is the same as the one after it.
+
+If it is, then we know that mid is not the single element, and that the single element must be at an even index after mid. Therefore, we set lo to be mid + 2. 
+It is +2 rather than the usual +1 because we want it to point at an even index.
+If it is not, then we know that the single element is either at mid, or at some index before mid. Therefore, we set hi to be mid.
+Once lo == hi, the search space is down to 1 element, and this must be the single element, so we return it.
+'''
+
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        lo = 0
+        hi = len(nums) - 1
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+            if mid % 2 == 1:
+                mid -= 1
+            if nums[mid] == nums[mid + 1]:
+                lo = mid + 2
+            else:
+                hi = mid
+        return nums[lo]
+"""
